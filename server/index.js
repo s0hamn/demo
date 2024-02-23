dcd679c7dd7a09e8d97985449c0818ed578c2ca3
Soham Naigaonkar <soham.naigaonkar@gmail.com>
2024-02-23 20:57:00 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -5,12 +5,20 @@ const User = require('./models/User');
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+// app.use(cors(
+//     {
+//         origin: []
+//     }
+// ));
 
 mongoose.connect('mongodb+srv://sohamnaigaonkar:soham123@cluster0.8urvkfy.mongodb.net/demodb')
 
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
 })
 
 app.post('/register', (req, res) => {
