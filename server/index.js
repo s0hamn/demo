d348953c3d767072eacd559f0d71c571d904820d
Soham Naigaonkar <soham.naigaonkar@gmail.com>
2024-02-23 21:23:00 +0530

    .

--- server/index.js
+++ server/index.js
@@ -13,6 +13,14 @@ app.use(express.json());
 //     }
 // ));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://demo-frontend-beryl-gamma.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+
 app.use(cors());
 mongoose.connect('mongodb+srv://sohamnaigaonkar:soham123@cluster0.8urvkfy.mongodb.net/demodb')
 
