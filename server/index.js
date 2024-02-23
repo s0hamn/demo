0ba2679c2fae6347b2722f0f315b9ca191884a2d
Soham Naigaonkar <soham.naigaonkar@gmail.com>
2024-02-23 22:39:20 +0530

    .

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(cors(
 
 
 // app.use(cors());
-mongoose.connect('mongodb+srv://sohamnaigaonkar:soham123@cluster0.8urvkfy.mongodb.net/demodb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://sohamnaigaonkar:soham123@cluster0.8urvkfy.mongodb.net/demodb?retryWrites=true&w=majority')
 
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
