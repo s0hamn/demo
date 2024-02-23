9ebbd4aa2342d1c723812f53e928c8b41d140315
Soham Naigaonkar <soham.naigaonkar@gmail.com>
2024-02-23 21:06:11 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/register' element={<Signup />} ></Route>
+        <Route path='/' element={<Signup />} ></Route>
       </Routes>
     </BrowserRouter>
   )
