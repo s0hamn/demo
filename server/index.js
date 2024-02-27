a7115160d1c874a9281324fdcf86fb975abbc3e5
Soham Naigaonkar <soham.naigaonkar@gmail.com>
2024-02-27 11:11:56 +0530

    ssssss

--- server/index.js
+++ server/index.js
@@ -42,10 +42,11 @@ app.get('/', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-    console.log(req.body.name)
+    // console.log(req.body.name)
     User.create({
         name: req.body.name
     }).then((user) => {
+
         res.json(user)
     }).catch((err) => {
         console.log(err)
