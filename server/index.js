41d70febf717f5facc8caa7e78dffc82f88f779d
Soham Naigaonkar <soham.naigaonkar@gmail.com>
2024-02-27 10:37:41 +0530

    .

--- server/index.js
+++ server/index.js
@@ -5,20 +5,24 @@ const User = require('./models/User');
 const app = express();
 app.use(express.json());
 
-app.use(cors(
-    {
-        origin: ["https://demo-frontend-beryl-gamma.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
+// app.use(cors(
+//     {
+//         origin: ["https://demo-frontend-beryl-gamma.vercel.app"],
+//         methods: ["POST", "GET"],
+//         credentials: true
+//     }
+// ));
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
+
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
+app.use(cors())
 
 
 // app.use(cors());
