c6120b0a15359f7d181e0cf2d17453b87f88ba79
Soham Naigaonkar <soham.naigaonkar@gmail.com>
2024-02-27 11:08:31 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -22,12 +22,12 @@ app.use(cors(
 //     next();
 // });
 
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 // app.use(cors());
