c584a8e4825e393ce24db00140ccfee05a84c1d9
Soham Naigaonkar <soham.naigaonkar@gmail.com>
2024-02-27 10:44:41 +0530

    ,

--- server/index.js
+++ server/index.js
@@ -22,10 +22,12 @@ app.use(express.json());
 //     next();
 // });
 
-app.use(cors({
-    origin: true,
-    credentials: true,
-}));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 // app.use(cors());
